9390169b0bbb3fb20acc03e55d4354177f30496c
spacebear <git@spacebear.dev>
2024-08-03 19:37:07 -0400

    add custom plugins
    added vim-surround, vim-illuminate, treesitter-context, and
    better-escape

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,23 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- replace surrounding pairs
+  'tpope/vim-surround',
+  -- highlight other uses of the word under the cursor
+  'RRethy/vim-illuminate',
+  {
+    -- add context on top of window
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      max_lines = 10,
+    },
+  },
+  {
+    -- <Esc> mappings
+    'max397574/better-escape.nvim',
+    config = function()
+      require('better_escape').setup()
+    end,
+  },
+}
