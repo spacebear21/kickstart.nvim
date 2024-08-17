0cef863e4489e7e7cd0fd191c70195a4dc3616dc
spacebear <git@spacebear.dev>
2024-08-17 18:41:40 -0400

    Add placeholder config for rust_analyzer cargo features
    It's helpful to enable certain features sometimes for development but I
    always forget the config syntax and have to spend minutes fiddling with
    it until I get it right, so here's a placeholder.

--- init.lua
+++ init.lua
@@ -582,7 +582,7 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         pyright = {},
-        rust_analyzer = {},
+        rust_analyzer = { settings = { ['rust-analyzer'] = { cargo = { features = '[]' } } } },
         html = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
