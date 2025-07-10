5cf212ed5f5d874bd9a06fdf195bee0c672b5dd9
spacebear <git@spacebear.dev>
2025-07-10 16:20:10 -0400

    use clangd for C files

--- init.lua
+++ init.lua
@@ -669,7 +669,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        clangd = { filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'hpp' } },
         -- gopls = {},
         pyright = {},
         rust_analyzer = { settings = { ['rust-analyzer'] = { cargo = { features = 'all' } } } },
